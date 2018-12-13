540bba85f7913ca75a98df927ae57313d5cb3295
Rémi Audebert <rflah0@gmail.com>
2018-12-13 18:41:47 +0100

    Update name to python-cellaserv3

---
+++ README.md
@@ -0,0 +1,22 @@
+# python-cellaserv3
+
+Python3 library for cellaserv3.
+
+## Install
+
+Requirements:
+
+Installing ``python-cellaserv3``::
+
+    $ git clone ssh://git@bitbucket.org/evolutek/python-cellaserv3.git
+    $ cd python-cellaserv3
+    $ git submodule init
+    $ git submodule update cellaserv/protobuf
+    $ python setup.py develop
+
+## Authors
+
+- Rémi Audebert, Evolutek 2012-2018
+- Benoît Reitz, Evolutek 2013-2015
+- Adrien Schildknecht, Evolutek 2013-2015
+- Vincent Gatine, Evolutek 2014-2015
--- setup.py
+++ setup.py
@@ -4,23 +4,19 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='python-cellaserv2',
+    name='python-cellaserv3',
     version='git',
-    url='code.evolutek.org/python-cellaserv2',
-    description='Python client for cellaserv2',
-    author='Remi Audebert - Evolutek 2013-2014',
-    author_email='contact@halfr.net',
-
+    url='https://bitbucket.org/evolutek/python-cellaserv3',
+    description='Python client for cellaserv3',
+    author='Evolutek - 2018',
+    author_email='evolutek@googlegroups.com',
     install_requires=open('requirements.txt').read().splitlines(),
-
     packages=['cellaserv', 'cellaserv.protobuf'],
-
     test_requires=['pytest', 'pytest-timeout'],
-
     classifiers=[
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
