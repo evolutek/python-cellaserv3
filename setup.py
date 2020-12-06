ad61fbedecb23125977158aa6e481b90847c98cd
Rémi Audebert <rflah0@gmail.com>
2020-12-06 17:02:05 +0100

    setup.py: use find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="python-cellaserv3",
     version="git",
     url="https://github.com/evolutek/python-cellaserv3",
@@ -11,9 +8,9 @@ setup(
     author="Evolutek",
     author_email="evolutek@googlegroups.com",
     install_requires=open("requirements.txt").read().splitlines(),
-    packages=["cellaserv", "cellaserv.protobuf"],
-    tests_require=["pytest", "pytest-asyncio"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
     ],
+    python_requires=">=3.8",
 )
